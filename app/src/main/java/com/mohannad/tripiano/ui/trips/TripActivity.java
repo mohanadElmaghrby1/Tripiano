ffe429d11342fb9e4bdfc2a86acf545fa083a205
mohannad <mohanad2021996@gmail.com>
2019-11-20 22:57:53 +0200

    Create Trips Activity

---
+++ app/src/main/java/com/mohannad/tripiano/ui/trips/TripActivity.java
@@ -0,0 +1,16 @@
+package com.mohannad.tripiano.ui.trips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohannad.tripiano.R;
+
+public class TripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip);
+    }
+}
