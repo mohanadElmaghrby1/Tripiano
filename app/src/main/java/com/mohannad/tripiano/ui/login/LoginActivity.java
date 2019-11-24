9342c733f87291c70e80747427a35da0945d16b2
mohannad <mohanad2021996@gmail.com>
2019-11-24 23:45:23 +0200

    Create login activity

--- app/src/main/java/com/mohannad/tripiano/ui/login/LoginActivity.java
+++ app/src/main/java/com/mohannad/tripiano/ui/login/LoginActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 import com.google.android.material.snackbar.Snackbar;
 import com.mohannad.tripiano.R;
 import com.mohannad.tripiano.databinding.ActivityLoginBinding;
+import com.mohannad.tripiano.ui.registeration.RegisterActivity;
+import com.mohannad.tripiano.ui.trips.TripActivity;
 import com.mohannad.tripiano.utils.NetworkUtils;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,7 +33,6 @@ public class LoginActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("loading");
-        binding.registerbtn.setText("new only");
 
     }
 
@@ -34,10 +41,30 @@ public class LoginActivity extends AppCompatActivity {
             Snackbar.make(findViewById(R.id.scrollView),"please make sure that you are connected to intenrnet", Snackbar.LENGTH_SHORT).show();
             return ;
         }
+        onLoginButtonClicked();
+    }
 
+    private void onLoginButtonClicked() {
         showLoading();
+        Backendless.UserService.login(binding.etPhoneNumber.getText().toString(),
+                binding.etPass.getText().toString(),
+                new AsyncCallback<BackendlessUser>() {
+                    @Override
+                    public void handleResponse(BackendlessUser response) {
+                        Toast.makeText(LoginActivity.this, "email"+response.getObjectId(),
+                                Toast.LENGTH_SHORT).show();
+//                        loginSuccess();
+                        dismissLoading();
+                        openHome();
+                    }
 
-
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(LoginActivity.this,  fault.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                        dismissLoading();
+                    }
+                }, true);
     }
 
 
@@ -48,5 +75,16 @@ public class LoginActivity extends AppCompatActivity {
 
     private void dismissLoading() {
         progressDialog.dismiss();
+    }
+
+    public void register(View view) {
+        Intent intent =new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHome() {
+        Intent intent =new Intent(this, TripActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/mohannad/tripiano/ui/login/LoginViewModel.java
+++ app/src/main/java/com/mohannad/tripiano/ui/login/LoginViewModel.java
@@ -2,6 +2,13 @@ package com.mohannad.tripiano.ui.login;
 
 import androidx.lifecycle.ViewModel;
 
+import com.mohannad.tripiano.data.model.RequestCallback;
+
 public class LoginViewModel extends ViewModel {
 
+
+
+    public void login(String email, String password, RequestCallback requestCallback) {
+
+    }
 }
