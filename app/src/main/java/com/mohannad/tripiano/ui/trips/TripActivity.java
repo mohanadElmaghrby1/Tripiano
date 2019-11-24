90d0a8b52cb1b5d9947071c073c3014761479959
mohannad <mohanad2021996@gmail.com>
2019-11-24 23:47:49 +0200

    Create Trip Activity

--- app/src/main/java/com/mohannad/tripiano/ui/trips/TripActivity.java
+++ app/src/main/java/com/mohannad/tripiano/ui/trips/TripActivity.java
@@ -1,16 +1,70 @@
 package com.mohannad.tripiano.ui.trips;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.backendless.Backendless;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mohannad.tripiano.R;
+import com.mohannad.tripiano.data.model.Trip;
+import com.mohannad.tripiano.databinding.ActivityTripBinding;
+import com.mohannad.tripiano.ui.registeration.RegisterActivity;
+import com.mohannad.tripiano.ui.trips.TripFragment.TripFragment;
+import com.mohannad.tripiano.ui.trips.addTrip.AddTripActivity;
+
+import java.util.List;
 
 public class TripActivity extends AppCompatActivity {
+
+    ActivityTripBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_trip);
+
+        binding.bottomNavigationView2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_coming_trips:
+                        fragment= new TripFragment(TripFragment.COMING);
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_past_trips:
+                        fragment= new TripFragment(TripFragment.DONE);
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_canceled_trips:
+                        fragment= new TripFragment(TripFragment.CANCELED);
+                        loadFragment(fragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void openAddTripActivity(View view) {
+        Intent intent =new Intent(this, AddTripActivity.class);
+        startActivity(intent);
     }
 }
